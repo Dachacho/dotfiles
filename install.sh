6b79315186d530d82f5a0b0ce8ba28d1a1b641db
Dachacho <arevadzedachi123@gmail.com>
2026-04-29 15:32:23 +0400

    fastfetch config

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ rm -f ~/.aerospace.toml
 rm -f ~/.config/starship.toml
 rm -rf ~/.config/ghostty
 rm -rf ~/.config/sketchybar
+rm -rf ~/.config/fastfetch
 
 #install
 ln -sf $DOTFILES/.zshrc ~/.zshrc
@@ -20,4 +21,6 @@ ln -sf $DOTFILES/.aerospace.toml ~/.aerospace.toml
 ln -sf $DOTFILES/starship.toml ~/.config/starship.toml
 ln -sf $DOTFILES/ghostty ~/.config/ghostty
 ln -sf $DOTFILES/sketchybar ~/.config/sketchybar
+ln -sf $DOTFILES/fastfetch ~/.config/fastfetch
+
 echo "Done!"
