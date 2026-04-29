2fa82f06e904b939209c15b63457f60dc434906a
Dachacho <arevadzedachi123@gmail.com>
2026-04-29 18:33:24 +0400

    optimize

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# Only update prev and current workspace (2 items instead of 5)
+# Fast update - only change colors, no aerospace calls
 sketchybar \
   --set "space.$FOCUSED_WORKSPACE" \
     background.color=0xfffabd2f \
-    icon.drawing=off \
-    label.drawing=on \
-    label="$FOCUSED_WORKSPACE" \
+    icon.color=0xff282828 \
     label.color=0xff282828 \
   --set "space.$AEROSPACE_PREV_WORKSPACE" \
     background.color=0xff3c3836 \
-    icon.drawing=on \
-    icon="$AEROSPACE_PREV_WORKSPACE" \
     icon.color=0xffa89984 \
-    label.drawing=off
+    label.color=0xffa89984
