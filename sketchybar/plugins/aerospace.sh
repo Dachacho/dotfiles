dbaf7bd5ba5ce4443369ba82bfd346b662459634
Dachacho <arevadzedachi123@gmail.com>
2026-05-12 18:49:09 +0400

    fix topbar

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Fast update - only change colors, no aerospace calls
+# Carbonfox Ghostty colors
 sketchybar \
   --set "space.$FOCUSED_WORKSPACE" \
     background.color=0xff78a9ff \
@@ -8,5 +8,5 @@ sketchybar \
     label.color=0xff161616 \
   --set "space.$AEROSPACE_PREV_WORKSPACE" \
     background.color=0xff282828 \
-    icon.color=0xff525253 \
-    label.color=0xff525253
+    icon.color=0xff484848 \
+    label.color=0xff484848
