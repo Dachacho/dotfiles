2b2b7d914d87f75aaf24411c7a603836800ba063
Dachacho <arevadzedachi123@gmail.com>
2026-05-12 18:40:21 +0400

    switched to carbonfox

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -3,10 +3,10 @@
 # Fast update - only change colors, no aerospace calls
 sketchybar \
   --set "space.$FOCUSED_WORKSPACE" \
-    background.color=0xfffabd2f \
-    icon.color=0xff282828 \
-    label.color=0xff282828 \
+    background.color=0xff78a9ff \
+    icon.color=0xff161616 \
+    label.color=0xff161616 \
   --set "space.$AEROSPACE_PREV_WORKSPACE" \
-    background.color=0xff3c3836 \
-    icon.color=0xffa89984 \
-    label.color=0xffa89984
+    background.color=0xff282828 \
+    icon.color=0xff525253 \
+    label.color=0xff525253
